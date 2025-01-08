b9a2aa17a71bdc96ad3bc42c85b016e3a13502ff
MetteTobiasen <tobiasen_1@hotmail.com>
2025-01-08 22:36:10 +0100

    tilføjet profile component og vist på profil siden

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -1,6 +1,39 @@
 <template>
     <main class="profil-page">
-        <h1>Profil</h1>
-        <p>This is the profil page</p>
+        <profile-form :user="user" v-if="user"></profile-form>
     </main>
 </template>
+
+<script>
+const UrlCompany = "https://localhost:7070/api/Company";
+
+import ProfileForm from '../components/ProfileForm.vue';
+import axios from 'axios';
+
+        export default {
+            components: {
+                ProfileForm,
+            },
+            data() {
+                return {
+                    user: "",
+                };
+            },
+            created() {
+                this.fetchUser();
+            },
+            methods: {
+                async fetchUser() {
+                    try {
+                        const url = UrlCompany + "/" + 2;
+                        const response = await axios.get(url);
+                        this.user = response.data;
+                    } catch (error) {
+                        console.error('Error fetching user:', error);
+                    }
+                },
+            },
+        };
+</script>
+
+
